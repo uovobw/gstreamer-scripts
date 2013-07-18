ee296a2ecfdb4d16acd85861e9e73dd024d68dc4
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-18 17:41:21 +0200

    fix alsa and speex params

--- sender_H264_SPEEX_010.sh
+++ sender_H264_SPEEX_010.sh
@@ -8,8 +8,8 @@ gst-launch-0.10 -v gstrtpbin name=rtpbin \
     rtpbin.send_rtp_src_0 ! udpsink host=$DST port=5000                            \
     rtpbin.send_rtcp_src_0 ! udpsink host=$DST port=5001 sync=false async=false    \
     udpsrc port=5005 ! rtpbin.recv_rtcp_sink_0                           \
-    audiotestsrc is-live=true ! audio/x-raw-int ! audiorate ! audioresample ! \
-    speexenc bitrate=8000 ! rtpspeexpay ! rtpbin.send_rtp_sink_1                   \
+    alsasrc device="default" ! audio/x-raw-int ! audiorate ! audioresample ! \
+    speexenc bitrate=24000 ! rtpspeexpay ! rtpbin.send_rtp_sink_1                   \
     rtpbin.send_rtp_src_1 ! udpsink host=$DST port=5002                            \
     rtpbin.send_rtcp_src_1 ! udpsink host=$DST port=5003 sync=false async=false    \
     udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1
