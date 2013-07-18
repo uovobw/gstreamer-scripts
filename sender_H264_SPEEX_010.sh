146c24555d053e7622ca2504fd6706f82f37adac
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-18 17:25:57 +0200

    Fixed some changes, added different versions for gstreamer 0.10 and 1.0

---
+++ sender_H264_SPEEX_010.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DST=${1:-pair07}
+
+gst-launch-0.10 -v gstrtpbin name=rtpbin \
+    v4l2src ! video/x-raw-yuv,width=\(int\)320,height=\(int\)240,framerate=\(fraction\)30/1 ! ffmpegcolorspace ! \
+    x264enc pass=cbr quantizer=20 bitrate=256 tune=zerolatency ! rtph264pay ! rtpbin.send_rtp_sink_0 \
+    rtpbin.send_rtp_src_0 ! udpsink host=$DST port=5000                            \
+    rtpbin.send_rtcp_src_0 ! udpsink host=$DST port=5001 sync=false async=false    \
+    udpsrc port=5005 ! rtpbin.recv_rtcp_sink_0                           \
+    audiotestsrc is-live=true ! audio/x-raw-int ! audiorate ! audioresample ! \
+    speexenc bitrate=8000 ! rtpspeexpay ! rtpbin.send_rtp_sink_1                   \
+    rtpbin.send_rtp_src_1 ! udpsink host=$DST port=5002                            \
+    rtpbin.send_rtcp_src_1 ! udpsink host=$DST port=5003 sync=false async=false    \
+    udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1
