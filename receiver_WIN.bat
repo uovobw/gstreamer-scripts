4f048f68ee2b89c03eb2d3690b076b7cfa163abd
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-23 11:56:51 +0200

    Fix typos, switch to autoaudiosink due to dshow and directsound being hard to guess

--- receiver_WIN.bat
+++ receiver_WIN.bat
@@ -3,7 +3,7 @@ set PATH=%PATH%;C:\QDesk\Bin\VirtualWaiterFrontDesk
 set DST=192.168.17.45
 set SRC=192.168.18.211
 
-gst-launch-0.10.exe -v ^
+gst-launch-0.10.exe ^
         gstrtpbin name=rtpbin                                          ^
         udpsrc caps="application/x-rtp,media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264" ^
         port=5000 ! rtpbin.recv_rtp_sink_0                                ^
@@ -12,7 +12,7 @@ gst-launch-0.10.exe -v ^
         rtpbin.send_rtcp_src_0 ! udpsink host=%DST% port=5005 sync=false async=false        ^
         udpsrc caps="application/x-rtp,media=(string)audio, clock-rate=(int)16000, encoding-name=(string)SPEEX, encoding-params=(string)1, payload=(int)110" ^
         port=5002 ! rtpbin.recv_rtp_sink_1                                ^
-        rtpbin. ! rtpspeexdepay ! speexdec ! audioresample ! audioconvert ! directsoundsink sync=false async=false ^
+        rtpbin. ! rtpspeexdepay ! speexdec ! audioresample ! audioconvert ! autoaudiosink ^
         udpsrc port=5003 ! rtpbin.recv_rtcp_sink_1                               ^
         rtpbin.send_rtcp_src_1 ! udpsink host=%DST% port=5007 sync=false async=false
 
