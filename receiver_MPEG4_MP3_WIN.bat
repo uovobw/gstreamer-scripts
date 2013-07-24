db0e21ab69d95d4543eef186c3cfe91fd19c7c26
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-24 18:07:26 +0200

    Add ffmpegcolorspace and vvideoscale between decoder and sink Add send-config=true in rtpmpeg4vpay

--- receiver_MPEG4_MP3_WIN.bat
+++ receiver_MPEG4_MP3_WIN.bat
@@ -6,7 +6,7 @@ gst-launch-0.10.exe -v ^
         gstrtpbin name=rtpbin                                          ^
         udpsrc caps="application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)MP4V-ES" ^
         port=5000 ! rtpbin.recv_rtp_sink_0                                ^
-        rtpbin. ! rtpmp4vdepay ! ffdec_mpeg4  ! autovideosink sync=true async=true   ^
+        rtpbin. ! rtpmp4vdepay ! mpeg4videoparse ! ffdec_mpeg4 max-threads=1 ! ffmpegcolorspace ! videoscale  ! autovideosink sync=true async=true   ^
         udpsrc port=5001 ! rtpbin.recv_rtcp_sink_0  ^
         rtpbin.send_rtcp_src_0 ! udpsink host=%DST% port=5005 sync=false async=false^
         udpsrc caps="application/x-rtp,media=(string)audio, clock-rate=(int)90000, encoding-name=(string)MPA, encoding-params=(string)1" ^
