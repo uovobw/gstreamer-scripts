d7257bbfac7cc8b7bf15a65fd1a314a93e41905a
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-24 14:14:55 +0200

    added windows mp2 audio sender/receiver

---
+++ receiver_H264_MP2_WIN.bat
@@ -0,0 +1,17 @@
+rem set PATH=%PATH%;C:\QDesk\Bin\VirtualWaiterFrontDesk
+
+set DST=192.168.17.45
+
+gst-launch-0.10.exe -v ^
+        gstrtpbin name=rtpbin                                          ^
+        udpsrc caps="application/x-rtp,media=(string)video,clock-rate=(int)90000,encoding-name=(string)H264" ^
+        port=5000 ! rtpbin.recv_rtp_sink_0                                ^
+        rtpbin. ! rtph264depay ! h264parse ! ffdec_h264  ! autovideosink sync=true async=true   ^
+        udpsrc port=5001 ! rtpbin.recv_rtcp_sink_0  ^
+        rtpbin.send_rtcp_src_0 ! udpsink host=%DST% port=5005 sync=false async=false^
+        udpsrc caps="application/x-rtp,media=(string)audio, clock-rate=(int)24000, encoding-name=(string)SPEEX, encoding-params=(string)1" ^
+        port=5002 ! rtpbin.recv_rtp_sink_1                                ^
+        rtpbin. ! rtpmpadepay ! ffdec_mp2float ! audioresample ! audioconvert ! directsoundsink sync=true async=true^
+        udpsrc port=5003 ! rtpbin.recv_rtcp_sink_1                               ^
+        rtpbin.send_rtcp_src_1 ! udpsink host=%DST% port=5007 sync=false async=false
+
