02e2e8da5dc95c0af1caee7ea036d0c924e915f8
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-07-25 10:10:34 +0200

    Added self-view for the sending peer

--- sender_MPEG4_MP3_010.sh
+++ sender_MPEG4_MP3_010.sh
@@ -3,7 +3,7 @@
 DST=$1
 
 gst-launch-0.10 -v gstrtpbin name=rtpbin \
-    v4l2src ! video/x-raw-yuv,width=\(int\)320,height=\(int\)240,framerate=\(fraction\)30/1 ! ffmpegcolorspace ! \
+    v4l2src ! video/x-raw-yuv,width=\(int\)320,height=\(int\)240,framerate=\(fraction\)30/1 ! tee name="t" ! ffmpegcolorspace ! \
     videoscale ! videorate ! ffenc_mpeg4 max-key-interval=1 max-bframes=2 ! rtpmp4vpay send-config=true ! rtpbin.send_rtp_sink_0 \
     rtpbin.send_rtp_src_0 ! udpsink host=$DST port=5000 sync=false async=false   \
     rtpbin.send_rtcp_src_0 ! udpsink host=$DST port=5001 sync=false async=false    \
@@ -12,4 +12,5 @@ gst-launch-0.10 -v gstrtpbin name=rtpbin \
     lamemp3enc bitrate=128 target=0 quality=5 encoding-engine-quality=1 mono=true ! rtpmpapay ! rtpbin.send_rtp_sink_1                   \
     rtpbin.send_rtp_src_1 ! udpsink host=$DST port=5002 sync=false async=false  \
     rtpbin.send_rtcp_src_1 ! udpsink host=$DST port=5003 sync=false async=false    \
-    udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1
+    udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1 \
+    t. ! xvimagesink sync=true async=true
--- sender_MPEG4_MP3_WIN.bat
+++ sender_MPEG4_MP3_WIN.bat
@@ -3,7 +3,7 @@ set PATH=%PATH%;C:\QDesk\Bin\VirtualWaiterFrontDesk
 set DST=192.168.219.102
 
 gst-launch-0.10.exe -v gstrtpbin name=rtpbin ^
-    dshowvideosrc ! video/x-raw-yuv,width=(int)320,height=(int)240,framerate=(fraction)30/1 ! ffmpegcolorspace ! ^
+    dshowvideosrc ! video/x-raw-yuv,width=(int)320,height=(int)240,framerate=(fraction)30/1 ! ffmpegcolorspace ! tee name="t" ! ^
     videorate ! videoscale ! ffenc_mpeg4 max-key-interval=1 max-bframes=2 ! rtpmp4vpay send-config=true ! rtpbin.send_rtp_sink_0 ^
     rtpbin.send_rtp_src_0 ! udpsink host=%DST% port=5000 sync=false async=false ^
     rtpbin.send_rtcp_src_0 ! udpsink host=%DST% port=5001 sync=false async=false    ^
@@ -12,4 +12,5 @@ gst-launch-0.10.exe -v gstrtpbin name=rtpbin ^
     lamemp3enc bitrate=128 target=0 quality=5 encoding-engine-quality=1 mono=true ! rtpmpapay ! rtpbin.send_rtp_sink_1                   ^
     rtpbin.send_rtp_src_1 ! udpsink host=%DST% port=5002 sync=false async=false ^
     rtpbin.send_rtcp_src_1 ! udpsink host=%DST% port=5003 sync=false async=false    ^
-    udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1
+    udpsrc port=5007 ! rtpbin.recv_rtcp_sink_1 ^
+    t. ! autovideosink sync=true async=true
