fb71b75967b67a9796a8f41d8c05c30f763a2c4f
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2014-05-01 03:43:25 +0200

    Add script to stream webcam with audio to icecast server

---
+++ webcam-audio-to-icecast.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <ip> <port> <mountpoint>"
+    exit 1
+fi
+
+stty -echo
+printf "Password:"
+read ICECAST_PASSWORD
+stty echo
+printf "\n"
+
+gst-launch-1.0 v4l2src do-timestamp=true ! video/x-raw,width=\(int\)320,height=\(int\)240,framerate=\(fraction\)10/1 ! queue leaky=1 !\
+    theoraenc bitrate=400 quality=63 ! queue leaky=1 ! oggmux name=mux \
+    alsasrc ! queue leaky=1 ! audioconvert ! vorbisenc quality=0.1 ! mux. \
+    mux. ! queue leaky=1 ! shout2send ip=${1} port=${2} mount=/${3} password=${ICECAST_PASSWORD} sync=true async=true
