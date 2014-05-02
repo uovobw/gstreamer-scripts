e9b4f2416cccaf4fc47724c07485a5e56c480add
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2014-05-02 12:32:27 +0200

    reduce default quality as it is waaaaaay to much data for italian DSLs :(

--- webcam-audio-to-icecast.sh
+++ webcam-audio-to-icecast.sh
@@ -12,6 +12,6 @@ stty echo
 printf "\n"
 
 gst-launch-1.0 v4l2src do-timestamp=true ! video/x-raw,width=\(int\)320,height=\(int\)240,framerate=\(fraction\)10/1 ! queue leaky=1 !\
-    theoraenc bitrate=400 quality=63 ! queue leaky=1 ! oggmux name=mux \
+    theoraenc bitrate=400 quality=32 ! queue leaky=1 ! oggmux name=mux \
     alsasrc ! queue leaky=1 ! audioconvert ! vorbisenc quality=0.1 ! mux. \
     mux. ! queue leaky=1 ! shout2send ip=${1} port=${2} mount=/${3} password=${ICECAST_PASSWORD} sync=true async=true
